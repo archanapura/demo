91d8a64099b7efe30699cf5a40eee59b0e3d2829
archanapura <archanapura@gmail.com>
2018-04-24 17:59:20 +0530

    commit1

---
+++ advSeleniumScripts/src/test/java/advSelenium/advSeleniumScripts/AppTest.java
@@ -0,0 +1,13 @@
+package advSelenium.advSeleniumScripts;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void demmo() 
+	{
+		String browser1=System.getProperty("browser");
+		System.out.println(browser1);
+	}
+}
